95a1b1b24fd5802b08d436dad6ab7565af780fcd
R4JxF60 <rajithakumaraprog@gmail.com>
2022-04-15 11:11:53 +0530

    downgrade: react to 17.0.2

--- src/init.js
+++ src/init.js
@@ -1,1 +1,10 @@
-console.log(module.exports);
+import React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <h1>
+            Hello Workd from React 17.0.2
+        </h1>
+    </React.StrictMode>
+, document.getElementById("root"));
