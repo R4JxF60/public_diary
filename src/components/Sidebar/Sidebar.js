86d66518ab4b957a4dca16e46bb045d9c7b48b07
R4JxF60 <rajithakumaraprog@gmail.com>
2022-04-21 19:09:06 +0530

    update: sidebar components

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import TodayDiv from "./TodayDiv/TodayDiv";
 
 class Sidebar extends React.Component {
     constructor(props) {
@@ -8,7 +9,7 @@ class Sidebar extends React.Component {
     render() {
         return (
             <div className="order-last h-[10vh]  md:order-first lg:h-[calc(85vh-1.125rem)] md:h-[calc(88vh-1.125rem)] border border-solid bg-pd-pure-white border-pd-stroke-blue rounded-2xl mr-1.5 ml-1.5 mt-1.5 md:mb-1.5 dark:bg-pd-dark-fade-black dark:border-none">
-                <div className="lg:w-80 md:w-64 w-64 h-[64px]"></div>
+                <TodayDiv />
             </div>
         );
     }
